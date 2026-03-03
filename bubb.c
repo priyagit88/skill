c06d1d81dae354407299adc42f49655ab26e482f
Priya <priyatechh@gmail.com>
2026-03-03 11:11:02 +0530

    second commit p

--- bubb.c
+++ bubb.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
+int main() {
+    int arr[100], n, i, j, temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
 
