2826a2c4a8b4f305d40fc9cffbe5a5635bd46d63
Haricharanv <haricharanv2004@gmail.com>
2026-03-03 11:08:30 +0530

    third commit s

--- bubb.c
+++ bubb.c
@@ -9,4 +9,12 @@ int main() {
     for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
